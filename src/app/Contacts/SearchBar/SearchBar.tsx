ad782ffb414dbc66f395a8bb33697944728486b0
Benjamin Brandon <benb89.bb@gmail.com>
2024-03-03 04:03:56 -0500

    added boostrap basics

--- src/app/Contacts/SearchBar/SearchBar.tsx
+++ src/app/Contacts/SearchBar/SearchBar.tsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 import styles from './SearchBar.module.scss';
+import { FloatingLabel, Form, Button, Container } from 'react-bootstrap';
 
 const SearchBar = () => {
   return (
-    <div className={styles.SearchBarContainer}>
+    <Container className={styles.SearchBarContainer}>
       <h1>SearchBar</h1>
-    </div>
+    </Container>
   );
 };
+/*
+<FloatingLabel
+        controlId="floatingInput"
+        label="Email address"
+        className="mb-3"
+      >
+        <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingPassword" label="Password">
+        <Form.Control type="password" placeholder="Password" />
+      </FloatingLabel>
+*/
