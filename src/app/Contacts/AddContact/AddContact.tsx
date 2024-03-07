e79b0c75ea52253013cba5d7d6270f9cef65929c
Benjamin Brandon <benb89.bb@gmail.com>
2024-03-06 23:26:56 -0500

    adding styles files for add contact

--- src/app/Contacts/AddContact/AddContact.tsx
+++ src/app/Contacts/AddContact/AddContact.tsx
@@ -1,120 +1,134 @@
 import React from 'react';
-import { Container, FloatingLabel, Form, Row } from 'react-bootstrap';
+import {
+  Button,
+  Card,
+  CardBody,
+  Container,
+  FloatingLabel,
+  Form,
+  Row,
+} from 'react-bootstrap';
+import styles from './AddContact.module.scss';
 
 export const AddContact = () => {
   return (
     <Container>
-      <Form>
-        <Row md="3">
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="First name"
-          >
-            <Form.Control
-              type="text"
-              placeholder="First name"
-            />
-          </FloatingLabel>
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="Last name"
-          >
-            <Form.Control
-              type="text"
-              placeholder="Last name"
-            />
-          </FloatingLabel>
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="Middle name"
-          >
-            <Form.Control
-              type="text"
-              placeholder="Middle name"
-            />
-          </FloatingLabel>
-        </Row>
-        <Row md="3">
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="Home phone"
-          >
-            <Form.Control
-              type="text"
-              placeholder="Home phone"
-            />
-          </FloatingLabel>
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="Cell phone"
-          >
-            <Form.Control
-              type="text"
-              placeholder="Cell phone"
-            />
-          </FloatingLabel>
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="Work phone"
-          >
-            <Form.Control
-              type="text"
-              placeholder="Work phone"
-            />
-          </FloatingLabel>
-        </Row>
-        <Row md="2">
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="Street address"
-          >
-            <Form.Control
-              type="text"
-              placeholder="Street address"
-            />
-          </FloatingLabel>
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="City"
-          >
-            <Form.Control
-              type="text"
-              placeholder="City"
-            />
-          </FloatingLabel>
-        </Row>
-        <Row md="2">
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="State"
-          >
-            <Form.Control
-              type="text"
-              placeholder="State"
-            />
-          </FloatingLabel>
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="Zip code"
-          >
-            <Form.Control
-              type="text"
-              placeholder="Zip code"
-            />
-          </FloatingLabel>
-        </Row>
-        <Row>
-          <FloatingLabel
-            controlId="floatingSearch"
-            label="Birthday"
-          >
-            <Form.Control
-              type="date"
-              placeholder="Birthday"
-            />
-          </FloatingLabel>
-        </Row>
-      </Form>
+      <Card>
+        <CardBody>
+          <Form>
+            <Row md="3">
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="First name"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="First name"
+                />
+              </FloatingLabel>
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="Last name"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Last name"
+                />
+              </FloatingLabel>
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="Middle name"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Middle name"
+                />
+              </FloatingLabel>
+            </Row>
+            <Row md="3">
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="Home phone"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Home phone"
+                />
+              </FloatingLabel>
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="Cell phone"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Cell phone"
+                />
+              </FloatingLabel>
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="Work phone"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Work phone"
+                />
+              </FloatingLabel>
+            </Row>
+            <Row md="2">
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="Street address"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Street address"
+                />
+              </FloatingLabel>
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="City"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="City"
+                />
+              </FloatingLabel>
+            </Row>
+            <Row md="2">
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="State"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="State"
+                />
+              </FloatingLabel>
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="Zip code"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Zip code"
+                />
+              </FloatingLabel>
+            </Row>
+            <Row>
+              <FloatingLabel
+                controlId="floatingSearch"
+                label="Birthday"
+              >
+                <Form.Control
+                  type="date"
+                  placeholder="Birthday"
+                />
+              </FloatingLabel>
+            </Row>
+            <Button>Submit</Button>
+          </Form>
+        </CardBody>
+      </Card>
     </Container>
   );
 };
