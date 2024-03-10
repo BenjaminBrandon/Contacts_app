fd9ed0abd35fa2cd37fb710f927c8307b71687e9
Benjamin Brandon <benb89.bb@gmail.com>
2024-03-10 09:15:37 -0400

    added interface tsx for contact

---
+++ src/app/Contacts/AddContact/interface.tsx
@@ -0,0 +1,12 @@
+interface Contact {
+  firstName?: string;
+  lastName?: string;
+  middleName?: string;
+  homePhone?: number;
+  cellPhone?: number;
+  workPhone?: number;
+  streetAddress?: string;
+  city?: string;
+  state?: string;
+  zip?: number;
+}
