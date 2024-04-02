f2e2cf66c2daba0f154ca50cebfc18dd0c31bd3e
Benjamin Brandon <benb89.bb@gmail.com>
2024-04-02 18:50:03 -0400

    moving card body to new component

--- src/app/Contacts/ContactCard/contactCard.tsx
+++ src/app/Contacts/ContactCard/contactCard.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import { Card, CardBody, CardHeader, Container } from 'react-bootstrap';
 
 const contactCard = () => {
   return (
     <Container>
-      <Card></Card>
+      <Card>
+        <CardHeader>Example Contact Name</CardHeader>
+        <CardBody>
+          <ul>
+            <li>Home Phone Number</li>
+            <li>Cell Phone Number</li>
+            <li>Work Phone Number</li>
+            <li>Email</li>
+            <li>Street Address</li>
+            <li>City</li>
+            <li>State</li>
+            <li>Birthday</li>
+          </ul>
+        </CardBody>
+      </Card>
     </Container>
   );
 };
