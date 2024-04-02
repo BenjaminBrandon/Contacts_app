1dcf046983b39f2aba243f18a2d541c8e3243706
Benjamin Brandon <benb89.bb@gmail.com>
2024-04-02 18:31:21 -0400

    added new file and folder for component contactCard

---
+++ src/app/Contacts/ContactCard/contactCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, Container } from 'react-bootstrap';
+
+const contactCard = () => {
+  return (
+    <Container>
+      <Card></Card>
+    </Container>
+  );
+};
+
+export default contactCard;
