8f482e4116a8213cb2c42ec46848ad2db6fb4313
Benjamin Brandon <benb89.bb@gmail.com>
2024-04-04 20:24:09 -0400

    removed redunant code in contacts file

--- src/app/Contacts/Contacts.tsx
+++ src/app/Contacts/Contacts.tsx
@@ -19,21 +19,6 @@ const Contacts = () => {
       />
       <div>
         <ContactCard contact={contacts} />
-        <Card>
-          <CardHeader>Example Contact Name</CardHeader>
-          <CardBody>
-            <ul>
-              <li>Home Phone Number</li>
-              <li>Cell Phone Number</li>
-              <li>Work Phone Number</li>
-              <li>Email</li>
-              <li>Street Address</li>
-              <li>City</li>
-              <li>State</li>
-              <li>Birthday</li>
-            </ul>
-          </CardBody>
-        </Card>
       </div>
     </div>
   );
