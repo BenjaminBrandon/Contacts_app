4efdf385b71b48ce066c10fa7f9ba6ef4f5d3590
Benjamin Brandon <benb89.bb@gmail.com>
2024-04-24 22:57:07 -0400

    fixed props to contacts

--- src/app/Contacts/ContactCard/contactCard.tsx
+++ src/app/Contacts/ContactCard/contactCard.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Card, CardBody, CardHeader, Container } from 'react-bootstrap';
 
-const ContactCard = (nextContact: Contact) => {
+const ContactCard = ({ nextContact }: Contact) => {
   // const {firstName, lastName, } = {contact}
-  console.log('new contact:', nextContact);
-  console.log('new contact name:', nextContact.firstName);
+  // console.log('new contact:', nextContact);
+  // console.log('new contact name:', nextContact.firstName);
   return (
     <Container>
       <Card>
-        <CardHeader>{nextContact.nextContact.firstName}</CardHeader>
+        <CardHeader>{nextContact.firstName}</CardHeader>
         <CardBody>
           <ul>
             <li>Home Phone Number</li>
