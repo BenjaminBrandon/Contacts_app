7376658c2fe4b362d7262c574b90acb0eecb62fd
Benjamin Brandon <benb89.bb@gmail.com>
2024-05-02 23:02:46 -0400

    code cleanup on list

--- src/app/Contacts/ContactList/ContactList.tsx
+++ src/app/Contacts/ContactList/ContactList.tsx
@@ -13,22 +13,21 @@ const ContactList = (contacts: Contact) => {
 
   const sort = object => {
     let listSorted = object.sort((a, b) => {
-      const nameA = a.firstName.toUpperCase(); // ignore upper and lowercase
-      const nameB = b.firstName.toUpperCase(); // ignore upper and lowercase
+      const nameA = a.firstName.toUpperCase();
+      const nameB = b.firstName.toUpperCase();
       if (nameA < nameB) {
         return -1;
       }
       if (nameA > nameB) {
         return 1;
       }
-      // names must be equal
+
       return 0;
     });
     console.log(listSorted);
   };
 
   sort(list);
-  ////
 
   return (
     <Container>
