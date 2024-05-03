da260ae5766e2ab0cc21affe66dcf44e3210d2b3
Benjamin Brandon <benb89.bb@gmail.com>
2024-05-02 22:58:07 -0400

    rewrote sorting function for list

--- src/app/Contacts/ContactList/ContactList.tsx
+++ src/app/Contacts/ContactList/ContactList.tsx
@@ -12,10 +12,19 @@ const ContactList = (contacts: Contact) => {
   console.log('pre sorted list:', list);
 
   const sort = object => {
-    // let contacts = [];
-    let sorted = object.sort();
-    return sorted;
-    console.log('sorted', sorted);
+    let listSorted = object.sort((a, b) => {
+      const nameA = a.firstName.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.firstName.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      // names must be equal
+      return 0;
+    });
+    console.log(listSorted);
   };
 
   sort(list);
